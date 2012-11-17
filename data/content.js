4e1145a27ecdae96f8be150a8d512e5419b213b8
poying <poying.me@gmail.com>
2012-11-17 18:46:52 +0800

    符合 JSLint

--- data/content.js
+++ data/content.js
@@ -1,12 +1,16 @@
-if (top === self) {
-} else { // in the iframe
-[].forEach.call(document.querySelectorAll('#header + .contents a'), function(link) {
-	// handle the YouTube link
-	if (link.className == 'ogvideo meta') {
-		var video = link.querySelector('img').dataset.video;
-		if (/youtube.com\/v\//i.test(video))
-			link.href = video.replace(/youtube.com\/v\/([0-9a-zA-Z\-\_]+).*/i, "youtube.com/embed/$1");
-	}
-});
+/*jslint browser: true, regexp: true */
+/*global top, self*/
+
+if (top !== self) { // in the iframe
+	[].forEach.call(document.querySelectorAll('#header + .contents a'), function (link) {
+		'use strict';
+		// handle the YouTube link
+		if (link.className === 'ogvideo meta') {
+			var video = link.querySelector('img').dataset.video;
+			if (/youtube.com\/v\//i.test(video)) {
+				link.href = video.replace(/youtube.com\/v\/([0-9a-zA-Z\-\_]+).*/i, "youtube.com/embed/$1");
+			}
+		}
+	});
 
 }
